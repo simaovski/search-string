a884fe29c2a9e1d9bd33670fe0abf91f8d6d8f64
simaovsky <william_s94@hotmail.com>
2019-03-20 20:21:38 -0300

    add logic to ACM and ScienceDirect

--- scripts/libraries.js
+++ scripts/libraries.js
@@ -1,23 +1,33 @@
 var title = `"DOCUMENT-TITLE":`;
 var abstract = "ABSTRACT:";
 var keywords = "KEYWORDS:";
+var LIBRARY = 'ieee';
+
 
 function updateLibrary() {
-    var library = $('#library').val();
-    if (library === 'ieee') {
+    LIBRARY = $('#library').val();
+    if (LIBRARY === 'ieee') {
         title = `"DOCUMENT-TITLE":`;
         abstract = "ABSTRACT:";
         keywords = "KEYWORDS:";
+    } else if (LIBRARY === 'acm') {
+        title = `acmdlTitle:(+#key#)`;
+        abstract = "recordAbstract:(+#key#)";
+        keywords = "keywords.author.keyword:(+#key#)";
+    } else {
+        title = `TITLE(`;
+        abstract = "ABSTRACT(";
+        keywords = "KEYWORDS(";
     }
+    updateString()
 }
-
 
 function updateString() {
     var stringSearch = "(";
     for (var i = 1; i < lastTerm; i++) {
         var term = getTerm(i);
-        document.getElementById(`term-${i}`).setAttribute('value', term);
         var operator = getOperator(i);
+        updateInputValues(term, operator, i);
         term = configTerm(term);
         if (operator != undefined) {
             if (operator === 'AND')
@@ -25,17 +35,35 @@ function updateString() {
             else
                 stringSearch += ` ${operator} `;
         }
-        stringSearch += `#key# ${term}`;
+        if (LIBRARY === 'ieee')
+            stringSearch += `#key# ${term}`;
+        else
+            stringSearch += term;
     }
     stringSearch += ")";
     updateAllStrings(stringSearch);
 }
 
-function updateAllStrings(stringTitle) {
-    $('#string-title').html(stringTitle.replace(/#key#/g, title));
-    $('#string-abstract').html(stringTitle.replace(/#key#/g, abstract));
-    $('#string-keywords').html(stringTitle.replace(/#key#/g, keywords));
+function updateInputValues(term, operator, i) {
+    document.getElementById(`term-${i}`).setAttribute('value', term);
+    if (operator != undefined)
+        document.getElementById(`operator-${i}`).setAttribute('value', operator);
+}
 
+function updateAllStrings(stringSearch) {
+    if (LIBRARY === 'ieee') {
+        $('#string-title').html(stringSearch.replace(/#key#/g, title));
+        $('#string-abstract').html(stringSearch.replace(/#key#/g, abstract));
+        $('#string-keywords').html(stringSearch.replace(/#key#/g, keywords));
+    } else if (LIBRARY === 'acm') {
+        $('#string-title').html(title.replace(/#key#/g, stringSearch));
+        $('#string-abstract').html(abstract.replace(/#key#/g, stringSearch));
+        $('#string-keywords').html(keywords.replace(/#key#/g, stringSearch));
+    } else {
+        $('#string-title').html(stringSearch.replace(/[(]/g, title));
+        $('#string-abstract').html(stringSearch.replace(/[(]/g, abstract));
+        $('#string-keywords').html(stringSearch.replace(/[(]/g, keywords));
+    }
 }
 
 function getTerm(position) {
